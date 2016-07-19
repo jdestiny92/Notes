aefc13d9c7cb55d5a4287bf9c29d495ad9278778
Julian Biebelberg <jbiebelberg@gmail.com>
2016-07-19 18:09:43 -0400

    removed unnecessary files

--- Verizon Service Activation_files/appinit.js
+++
@@ -1,8 +0,0 @@
-// Where context = id || jQuery object || undefined.
-function appInit(context) {
-
-	// initialize app specific components/widgets/utilities
-	//codeSampleInit(context);
-	//smoothScrollInit(context);
-    stepsInit();
-}
